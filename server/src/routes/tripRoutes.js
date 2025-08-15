4cb4b47c7236d8ddfc08d94d8b5e7decaf0d57c8
mo-seoudi <moseoudi@hotmail.com>
2025-08-15 17:03:11 +0400

    Update tripRoutes.js

--- server/src/routes/tripRoutes.js
+++ server/src/routes/tripRoutes.js
@@ -31,7 +31,7 @@ router.get("/", async (req, res, next) => {
         createdByUser: { select: { id: true, name: true, email: true } },
         parent: { select: { id: true } },
         children: { select: { id: true } },
-   //     subTripDocs: true,  //need proper fixing
+        subTripDocs: true,  
       },
     });
 
@@ -197,4 +197,5 @@ router.post("/:id/subtrips", async (req, res, next) => {
 });
 
 export default router;
+
 
