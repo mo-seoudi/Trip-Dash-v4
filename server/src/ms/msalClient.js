11d9bf4ec5f0be7436e534d4fec9a3c2ca7d7d2a
mo-seoudi <moseoudi@hotmail.com>
2025-08-26 16:01:13 +0400

    Update msalClient.js

--- server/src/ms/msalClient.js
+++ server/src/ms/msalClient.js
@@ -1,12 +1,10 @@
 // server/src/ms/msalClient.js
-const { ConfidentialClientApplication } = require("@azure/msal-node");
+import { ConfidentialClientApplication } from "@azure/msal-node";
 
-const cca = new ConfidentialClientApplication({
+export const cca = new ConfidentialClientApplication({
   auth: {
     clientId: process.env.MS_API_CLIENT_ID,
     clientSecret: process.env.MS_API_CLIENT_SECRET,
     authority: `https://login.microsoftonline.com/${process.env.MS_TENANT_ID || "common"}`,
   },
 });
-
-module.exports = { cca };
