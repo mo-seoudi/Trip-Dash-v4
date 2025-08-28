a78776a7f62764a98e7eda034f2e375ff43feeb3
mo-seoudi <moseoudi@hotmail.com>
2025-08-28 15:40:58 +0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,19 @@
+// client/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./index.css"; // Or whatever your main CSS file is
+import "./index.css";
+
+// ✅ MSAL provider (Microsoft 365 auth for the SPA)
+import { MsalProvider } from "@azure/msal-react";
+import { msalInstance } from "./auth/msal"; // make sure this file exists as we set earlier
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
