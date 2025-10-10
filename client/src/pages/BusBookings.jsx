a729e9a8c6cf2f63c1baa370b0f379e4e2861d62
mo-seoudi <moseoudi@hotmail.com>
2025-10-10 08:32:22 +0400

    Update BusBookings.jsx

--- client/src/pages/BusBookings.jsx
+++ client/src/pages/BusBookings.jsx
@@ -1,4 +1,4 @@
-// client/src/pages/Bookings.jsx
+// client/src/pages/BusBookings.jsx
 import React, { useCallback, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
