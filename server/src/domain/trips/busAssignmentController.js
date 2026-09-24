2e5f17a4b3b6bd3ab9fa9d5a83e5b657aae3cab6
mo-seoudi <moseoudi@hotmail.com>
2026-09-24 16:04:36 +0400

    Expose bus assignment update and delete handlers

--- server/src/domain/trips/busAssignmentController.js
+++ server/src/domain/trips/busAssignmentController.js
@@ -1,24 +1,7 @@
 import { createOperationalService } from "../../runtime/operationalService.js";
 import { createBusAssignmentService } from "./busAssignmentService.js";
-
-function service(req) {
-  return createOperationalService(req.operational, createBusAssignmentService);
-}
-
-export async function listBusAssignments(req, res, next) {
-  try {
-    const assignments = await service(req).list(req.params.tripId);
-    return res.json({ data: assignments });
-  } catch (error) {
-    return next(error);
-  }
-}
-
-export async function createBusAssignment(req, res, next) {
-  try {
-    const assignment = await service(req).create(req.params.tripId, req.body || {});
-    return res.status(201).json({ data: assignment });
-  } catch (error) {
-    return next(error);
-  }
-}
+function service(req){return createOperationalService(req.operational,createBusAssignmentService);}
+export async function listBusAssignments(req,res,next){try{return res.json({data:await service(req).list(req.params.tripId)});}catch(error){return next(error);}}
+export async function createBusAssignment(req,res,next){try{return res.status(201).json({data:await service(req).create(req.params.tripId,req.body||{})});}catch(error){return next(error);}}
+export async function updateBusAssignment(req,res,next){try{return res.json({data:await service(req).update(req.params.tripId,req.params.assignmentId,req.body||{})});}catch(error){return next(error);}}
+export async function deleteBusAssignment(req,res,next){try{await service(req).remove(req.params.tripId,req.params.assignmentId);return res.status(204).end();}catch(error){return next(error);}}
